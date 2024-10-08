98b06b06c818ef877b71db81423f305dae206da0
Lipeeeeeee <favasconcelos09@gmail.com>
2024-10-07 21:36:31 -0300

    projeto mini-game com front-end

---
+++ mini-game/README.md
@@ -0,0 +1,8 @@
+# Mini-game com front-end
+
+&emsp;Após aprender sobre HTML, CSS e javascript, criei um jogo de velocidade e reflexo para treinar mais minhas habilidades no front-end e conhecer as funções do JavaScript.<br>
+
+- O jogo consiste em acertar com o click do mouse o troféu da Bola de Ouro, premiada para o melhor jogador da temporada, durante 40 segundos, ganhando 1 ponto por click ou, realizando um combo de 10 clicks seguidos ou mais, sua pontuação por click dobra.
+- O jogador terá 3 vidas, perdendo uma vida clicando fora do espaço da Bola de Ouro. Após perder as 3 vidas, o jogo para e sua pontuação final aparece na tela.
+
+&emsp;Agora, após completar o jogo, acompanhe o resultado do meu jogo temático de futebol funcionando [aqui]()!
---
+++ mini-game/assets/script/script.js
@@ -0,0 +1,68 @@
+const valores = {
+    values: {
+        tempo: 40,
+        pontuacao: 0,
+        vidas: 3,
+        idBallon: 0,
+        combo: 0,
+    },
+    views: {
+        ballonDor: document.querySelector(".ballon-dor"),
+        quadrados: document.querySelectorAll(".quadrado"),
+        titulo: document.querySelector("#titulo"),
+        temporizador: document.querySelector("#temporizador"),
+        pontos: document.querySelector("#pontos"),
+        vidaTotal: document.querySelector("#vida")
+    }
+}
+
+function moverBallon(){
+    if(valores.values.tempo != 0){
+        valores.views.quadrados.forEach((quadrado) => {
+            quadrado.classList.remove("ballon-dor")
+        })
+        do{
+            valores.values.idBallon = Math.floor(Math.random() * 9)
+        }while(valores.views.ballonDor == valores.views.quadrados[valores.values.idBallon])
+        let novoBallonDor = valores.views.quadrados[valores.values.idBallon]
+        novoBallonDor.classList.add("ballon-dor")
+        valores.views.ballonDor = novoBallonDor
+    }
+}
+
+function contagemRegressiva(){
+    if(valores.values.tempo == 0 || valores.values.vidas == 0){
+        valores.views.titulo.textContent = (valores.values.pontuacao >= 10) ? `Parabéns, sua pontuação foi de ${valores.values.pontuacao} pontos, VOCÊ É O NOSSO BOLA DE OURO!` : `Sua pontuação foi de ${valores.values.pontuacao} pontos, que pena... Essa temporada não foi sua.`
+        clearInterval(movimento)
+        clearInterval(contagem)
+    }
+    if(valores.values.combo == 10) valores.views.titulo.textContent = "Você está com TUDO!! Pontuação dobrada"
+    else{
+        valores.values.tempo--
+        valores.views.temporizador.textContent = valores.values.tempo
+    }
+}
+
+function pontuar(){
+    valores.views.quadrados.forEach((quadrado) => {
+        quadrado.addEventListener("mousedown", () => {
+            if(quadrado == valores.views.ballonDor){
+                valores.values.pontuacao = (valores.values.combo >= 10) ? valores.values.pontuacao + 2 : valores.values.pontuacao + 1
+                ++valores.values.combo
+            }
+            else{
+                valores.values.combo = 0
+                valores.values.pontuacao -= 3
+                valores.values.vidas--
+                if(valores.values.vidas == 0) contagemRegressiva()
+                else valores.views.titulo.textContent = `Tome cuidado, você só tem ${valores.values.vidas} vida agora!`
+            }
+            valores.views.pontos.textContent = valores.values.pontuacao
+            valores.views.vidaTotal.textContent = `x${valores.values.vidas}`
+        })
+    })
+}
+
+pontuar()
+var movimento = setInterval(moverBallon, 600)
+var contagem = setInterval(contagemRegressiva, 1000)
