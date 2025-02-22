16a27c3207fb8041038d072d35ff6ceeaa8ba21f
Matheus Alves <129554334+MatheusADV@users.noreply.github.com>
2025-02-22 15:45:05 -0300

    feat: adicionando rotas de navegação

--- projeto-ria/src/app/app.routes.ts
+++ projeto-ria/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FormJogadorComponent } from './form-jogador/form-jogador.component';
+import { ListaJogadoresComponent } from './lista-jogadores/lista-jogadores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'jogadores', component: ListaJogadoresComponent, pathMatch: 'full' },
+  { path: 'jogadores/novo', component: FormJogadorComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/jogadores' }
+];
